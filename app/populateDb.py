a1de73bbae72cbbcefaf8d9c9f71f4f413f22ce1
Musa Qureshi <musaqureshi2002@gmail.com>
2023-12-08 22:05:29 +0000

    Handle discogs API rate limit 429

--- app/populateDb.py
+++ app/populateDb.py
@@ -1,26 +1,38 @@
 import sqlite3
 import time
 from discogsAccess import discogsManager
+from discogs_client.exceptions import HTTPError
 import json
-from config import COLOR
+from config import COLOR, loadingBar
 from track import Track
 from db import dbManager
 
 def get_tracks(startPage=1, endPage=5):
-    tracks = discogsManager.getTracks(startPage=startPage, endPage=endPage)
-    return tracks
+    if startPage > endPage:
+        print(f"{COLOR.FAIL}[Error]{COLOR.ENDC} End page is larger than start page")
+    else:
+        tracks = discogsManager.getTracks(startPage=startPage, endPage=endPage)
+        return tracks
 
 
 def populate(tracks, commit_interval=25):
     print(f"{COLOR.CYAN}Commit interval set to {commit_interval}{COLOR.ENDC}")
     for trackNum, musicTrack in enumerate(tracks):
-        tempTrack = Track()
-        tempTrack.extractData(musicTrack)
-        dbManager.addRow(tempTrack)
+        try:
+            tempTrack = Track()
+            tempTrack.extractData(musicTrack)
+            dbManager.addRow(tempTrack)
 
-        if (trackNum + 1) % commit_interval == 0:
-            dbManager.connection.commit()
-            print(f"{COLOR.GREEN}[Committed]{COLOR.ENDC} {commit_interval} tracks to the database")
+            if (trackNum + 1) % commit_interval == 0:
+                dbManager.connection.commit()
+                print(f"{COLOR.GREEN}[Committed]{COLOR.ENDC} {commit_interval} tracks to the database")
+
+        except HTTPError as e:
+            if e.status_code == 429:
+                print(f"{COLOR.FAIL}[Error]{COLOR.ENDC} Rate limit hit - timeout for {COLOR.CYAN}60{COLOR.ENDC} seconds")
+                loadingBar(60)
+            else:
+                raise
 
     dbManager.connection.commit()
     print(f"{COLOR.GREEN}[Committed]{COLOR.ENDC} remaining tracks to the database")
